6256438c69d991f6f621963c13ddd2a036704e4a
Dmitry Stupin <dm.stup@mail.ru>
2024-10-16 13:00:15 +0700

    Создание модуля avatar.js

---
+++ js/avatar.js
@@ -0,0 +1,6 @@
+const fileChoser = document.querySelector('#upload-file');
+const preview = document.querySelector('.img-upload__preview img');
+
+fileChoser.addEventListener('change', () => {
+  
+})
